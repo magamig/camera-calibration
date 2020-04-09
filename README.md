c96d0049ed21bb234cb47451645bd2086a282de7
magamig <magamig@gmail.com>
2020-04-09 15:20:15 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# camera-calibration
-Camera Calibration with OpenCV
+# Camera Calibration with OpenCV
+
+Read more about [the Intrinsic Matrix here](http://ksimek.github.io/2013/08/13/intrinsic/).
+
+## Instructions
+- Install python 3
+- `pip install -r requirements.txt`
+- `python main.py` 
+
+It will pop up a window. What you need to do is to move the 8x8 checkerboard in front of the camera until 30 frames is colleced. Make the variation of view angle & distance as large as possible. Camera intrinsic parameters will be computed and stored in `camera_parameters.json`.
+
+## Sample Output
+
+![](screenshot.png)
+
+
+```
+Valid Frame: 1/30
+Valid Frame: 2/30
+...
+Valid Frame: 30/30
+[[920.68754397   0.         679.63443165]
+ [  0.         920.33167683 361.67607775]
+ [  0.           0.           1.        ]]
+Total error:  0.058103822447327494
+```
+
+**Note:** code adapted from [ARchess](https://github.com/zuoym15/ARchess)
---
+++ main.py
@@ -0,0 +1,87 @@
+import json
+
+import cv2
+import numpy as np
+
+class FrameReader:
+    def __init__(self, width=1280, height=720, camera_id=0):
+        self.width = width
+        self.height = height
+        self.capture = cv2.VideoCapture(camera_id)
+        self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, self.width)
+        self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, self.height)
+
+    def read(self):
+        ret, img = self.capture.read()
+        if not ret:
+            raise RuntimeError("fail to read frame!")
+        return img  # img in gbr format
+
+reader = FrameReader()
+
+# Define the chess board rows and columns
+rows = 7
+cols = 7
+
+# Set the termination criteria for the corner sub-pixel algorithm
+criteria = cv2.TERM_CRITERIA_MAX_ITER + cv2.TERM_CRITERIA_EPS, 30, 0.001
+
+# Prepare the object points: (0,0,0), (1,0,0), (2,0,0), ..., (6,5,0). They are the same for all images
+objectPoints = np.zeros((rows * cols, 3), np.float32)
+objectPoints[:, :2] = np.mgrid[0:rows, 0:cols].T.reshape(-1, 2)
+
+# Create the arrays to store the object points and the image points
+objectPointsArray = []
+imgPointsArray = []
+
+valid_frame = 0
+
+while True:
+    # Load the image and convert it to gray scale
+    img = reader.read()
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Find the chess board corners
+    ret, corners = cv2.findChessboardCorners(gray, (rows, cols), None)
+
+    # Make sure the chess board pattern was found in the image
+    if ret:
+        # Refine the corner position
+        corners = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+
+        # Add the object points and the image points to the arrays
+        objectPointsArray.append(objectPoints)
+        imgPointsArray.append(corners)
+
+        # Draw the corners on the image
+        cv2.drawChessboardCorners(img, (rows, cols), corners, ret)
+
+        piece = 'Valid Frame: ' + str(valid_frame + 1) + '/30'
+
+        print(piece)
+
+        valid_frame += 1
+
+    # Display the image
+    cv2.imshow('chess board', img)
+    cv2.waitKey(1000)
+
+    if valid_frame >= 30:
+        break
+
+# Calibrate the camera and save the results
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objectPointsArray, imgPointsArray, gray.shape[::-1], None, None)
+print(mtx)
+
+# Print the camera calibration error
+error = 0
+
+for i in range(len(objectPointsArray)):
+    imgPoints, _ = cv2.projectPoints(objectPointsArray[i], rvecs[i], tvecs[i], mtx, dist)
+    error += cv2.norm(imgPointsArray[i], imgPoints, cv2.NORM_L2) / len(imgPoints)
+
+print("Total error: ", error / len(objectPointsArray))
+
+with open('./camera_parameters.json', 'w') as f:
+    json.dump(mtx.tolist(), f)
